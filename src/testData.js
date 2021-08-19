cf3cf2e7f4e8c23815aeaa2cd38c241b49972d8d
Lew <lewis.reid148@gmail.com>
2021-08-19 18:25:03 +0100

    testdata

---
+++ src/testData.js
@@ -0,0 +1,182 @@
+export const testExercisesInCategories = [
+    {
+        "name": "Chest",
+        "exercises": [
+            {
+                "name": "DB Fly",
+                "weight": 12,
+                "isDouble": true,
+                "reps": 15,
+                "sets": 3,
+                "maxWeight": 15 
+            },
+            {
+                "name": "DB Press",
+                "weight": 22,
+                "isDouble": true,
+                "reps": 12,
+                "sets": 3,
+                "maxWeight": 28 
+            },
+            {
+                "name": "Bench Press",
+                "weight": 50,
+                "isDouble": false,
+                "reps": 8,
+                "sets": 3,
+                "maxWeight": 70
+            }
+        ]
+
+    },
+    {
+        "name": "Core",
+        "exercises": [
+            {
+                "name": "Ab Crunch Machine",
+                "weight": 20,
+                "isDouble": false,
+                "reps": 12,
+                "sets": 4,
+                "maxWeight": null
+            },
+            {
+                "name": "Elevated Russian Twists",
+                "weight": 25,
+                "isDouble": false,
+                "reps": 16,
+                "sets": 3,
+                "maxWeight": null 
+            },
+            {
+                "name": "Suspended Leg Lifts",
+                "weight": 7,
+                "isDouble": false,
+                "reps": 12,
+                "sets": 3,
+                "maxWeight": null
+            }
+        ]
+
+    },
+    {
+        "name": "Core",
+        "exercises": [
+            {
+                "name": "Ab Crunch Machine",
+                "weight": 20,
+                "isDouble": false,
+                "reps": 12,
+                "sets": 4,
+                "maxWeight": null
+            },
+            {
+                "name": "Elevated Russian Twists",
+                "weight": 25,
+                "isDouble": false,
+                "reps": 16,
+                "sets": 3,
+                "maxWeight": null 
+            },
+            {
+                "name": "Suspended Leg Lifts",
+                "weight": null,
+                "isDouble": false,
+                "reps": 12,
+                "sets": 3,
+                "maxWeight": null
+            }
+        ]
+
+    },
+    {
+        "name": "Core",
+        "exercises": [
+            {
+                "name": "Ab Crunch Machine",
+                "weight": 20,
+                "isDouble": false,
+                "reps": 12,
+                "sets": 4,
+                "maxWeight": null
+            },
+            {
+                "name": "Elevated Russian Twists",
+                "weight": 25,
+                "isDouble": false,
+                "reps": 16,
+                "sets": 3,
+                "maxWeight": null 
+            },
+            {
+                "name": "Suspended Leg Lifts",
+                "weight": null,
+                "isDouble": false,
+                "reps": 12,
+                "sets": 3,
+                "maxWeight": null
+            }
+        ]
+
+    },
+    {
+        "name": "Core",
+        "exercises": [
+            {
+                "name": "Ab Crunch Machine",
+                "weight": 20,
+                "isDouble": false,
+                "reps": 12,
+                "sets": 4,
+                "maxWeight": null
+            },
+            {
+                "name": "Elevated Russian Twists",
+                "weight": 25,
+                "isDouble": false,
+                "reps": 16,
+                "sets": 3,
+                "maxWeight": null 
+            },
+            {
+                "name": "Suspended Leg Lifts",
+                "weight": null,
+                "isDouble": false,
+                "reps": 12,
+                "sets": 3,
+                "maxWeight": null
+            }
+        ]
+
+    },
+    {
+        "name": "Core",
+        "exercises": [
+            {
+                "name": "Ab Crunch Machine",
+                "weight": 20,
+                "isDouble": false,
+                "reps": 12,
+                "sets": 4,
+                "maxWeight": null
+            },
+            {
+                "name": "Elevated Russian Twists",
+                "weight": 25,
+                "isDouble": false,
+                "reps": 16,
+                "sets": 3,
+                "maxWeight": null 
+            },
+            {
+                "name": "Suspended Leg Lifts",
+                "weight": null,
+                "isDouble": false,
+                "reps": 12,
+                "sets": 3,
+                "maxWeight": null
+            }
+        ]
+
+    }
+]
